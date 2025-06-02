19b8e63945f3075825852e04d231acd9f176dc8b
vyshnavi <vyshnavichennagari2@gmail.com>
2025-06-02 16:35:47 +0530

    conversation started

--- World.sh
+++ World.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
  
- echo "Hi I am vyshnavi"
+ #echo "Hi I am vyshnavi"
 
+# echo "PERSON1:: HEY PERSON2 HELLO"
+ #echo "PERSON2:: HEY PERSON1 HI"
+
+ Number=$1
+
+ if [ $Number -lt 20 ]
+ then 
+     echo "$Number is greater than 200"
+ else
+      echo "$Number is less than 200"
