0c74fd307536281d2309c702f24ffdf94e0e9978
vyshnavi <vyshnavichennagari2@gmail.com>
2025-06-02 16:10:12 +0530

    conversation started

--- datatypes.sh
+++ datatypes.sh
@@ -6,6 +6,6 @@ if [ $Number -lt 10 ]
 then
   echo "Given number $Number is greater than 10"
 else
-  echo "given number $Number is leess tahnn 10"
+  echo "given number $Number is leess than 10"
 fi  
 
